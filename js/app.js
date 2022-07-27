8bc5d8e0bb22fb98972b6b5c2ef70d5d303e7305
Sansone11 <sansonemirco@gmail.com>
2022-07-27 19:22:27 +0200

    variabile-review

--- js/app.js
+++ js/app.js
@@ -1,19 +1,20 @@
 console.log('JS ok');
 const listaSpesa = ['pane 60g', 'latte 1lt', 'uova 6', 'sale 1kg', 'pepe 50g', 'pasta 500g', 'mele 6', 'miele 1l', 'farina 1kg',]
 console.log(listaSpesa);
+ //creo una variabile dichiaro un elemento da id che voglio utilizzare nell html
+ const text = document.getElementById('for');
 // condizione per ogni valore di i iferiore a length somma 1
 for (let i = 0; i < 9; i++) {
-    //creo una variabile dichiaro un elemento da id che voglio utilizzare nell html
-    const text = document.getElementById('for');
     //attacco in html il valore dell'indice in un list item html    
     text.innerHTML += "<li>" + (listaSpesa [i]) + "</li>";
 }
 
 let i = 0;
+ //creo una variabile dichiaro un elemento da id che voglio utilizzare nell html
+ const textList = document.getElementById('opposite');
 // condizione i deve essere minore delle length di listaspesa
 while (i < listaSpesa.length) {
-    //creo una variabile dichiaro un elemento da id che voglio utilizzare nell html
-    const textList = document.getElementById('opposite');
+   
     //attaacco in html il valore dell'indice in un list item html  
     textList.innerHTML += "<li>" + (listaSpesa[i]) + "</li>";
     i++;
