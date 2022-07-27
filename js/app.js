a65389c930a3a6cc815833a3f825639e005ae291
Sansone11 <sansonemirco@gmail.com>
2022-07-27 16:02:48 +0200

    dom-edit-bis

--- js/app.js
+++ js/app.js
@@ -1,22 +1,24 @@
 console.log('JS ok');
-const listaSpesa = ['pane 60g','latte 1lt','uova 6','sale 1kg','pepe 50g','pasta 500g','mele 6','miele 1l','farina 1kg',]
+const listaSpesa = ['pane 60g', 'latte 1lt', 'uova 6', 'sale 1kg', 'pepe 50g', 'pasta 500g', 'mele 6', 'miele 1l', 'farina 1kg',]
 console.log(listaSpesa);
- // condizione per ogni valore di i iferiore a length somma 1
- for (let i = 0; i < 9; i++) {
-    // dichiaro un elemento da id che voglio utilizzare nell html
-   const text = document.getElementById('for') 
-     text.innerHtml +="<li>"+(i + 1)+"</li>";
+// condizione per ogni valore di i iferiore a length somma 1
+for (let i = 0; i < 9; i++) {
+    //creo una variabile dichiaro un elemento da id che voglio utilizzare nell html
+    const text = document.getElementById('for');
+    //attaacco in html il valore dell'indice in un list item html    
+    text.innerHtml += "<li>" + (listaSpesa [i]) + "</li>";
 }
 
- let i = 0;
-    // condizione i deve essere minore delle length di listaspesa
- while( i < listaSpesa.length){
-     i++;
-     // dichiaro un elemento da id che voglio utilizzare nell html
-     const textList = document.getElementById('opposite'); 
-     textList.innerHTML += "<li>"+(i + 1)+"</li>";
-    }
+let i = 0;
+// condizione i deve essere minore delle length di listaspesa
+while (i <= listaSpesa.length) {
+    //creo una variabile dichiaro un elemento da id che voglio utilizzare nell html
+    const textList = document.getElementById('opposite');
+    //attaacco in html il valore dell'indice in un list item html  
+    textList.innerHTML += "<li>" + (listaSpesa[i]) + "</li>";
+    i++;
+}
 
-  
+
 
 
